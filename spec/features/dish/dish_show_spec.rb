25be27d5c71fb173a2fe9659e2e8276a9fb839a4
Eric Hale <eric_hale@comcast.net>
2020-07-24 09:59:54 -0600

    Add functionality to see a dishes' chef and ingredients on it's page

---
+++ spec/features/dish/dish_show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "When I visit a dish's show page" do
+    before(:each) do
+      @ramsey = Chef.create(name: "Gordon Ramsey")
+      @ingredient1 = Ingredient.create(name: "Spagetti", calories: 221)
+      ingredient2 = Ingredient.create(name: "Meatball", calories: 284)
+      dish_1 = @ramsey.dishes.create(name: "Spagetti and Meatballs", description: "Lovely Spagetti and meatballs")
+      DishIngredient.create(dish_id: dish_1.id, ingredient_id: @ingredient1.id)
+      DishIngredient.create(dish_id: dish_1.id, ingredient_id: ingredient2.id)
+    end
+    it "I see a list of ingredients for that dish and the chef's name" do
+
+
+      visit "/dishes/#{dish_1.id}"
+
+      within '.ingredients' do
+        expect(page).to have_content(@ingredient1.name)
+        expect(page).to have_content(ingredient2.name)
+      end
+      expect(page).to have_content(@ramsey.name)
+    end
+
+    it "@dish.chef.name" do
+
+    end
+  end
+end
