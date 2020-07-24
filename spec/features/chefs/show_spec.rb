882b08edeadb042e7ddd009043a85d6c8415b7bf
Eric Hale <eric_hale@comcast.net>
2020-07-24 10:44:44 -0600

    Add test for chef's ingredients index page

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -11,12 +11,19 @@ RSpec.describe "When I visit a chef's show page" do
   end
 
   it "I see the name of that chef and I see a link to view a list of all ingredients that this chef uses in their dishes" do
+
     visit "/chefs/#{@ramsey.id}"
 
     expect(page).to have_content(@ramsey.name)
     expect(page).to have_link("View Chef's Ingredients")
   end
+
+  it "When I click on that link I'm taken to a chef's ingredient index page and I can see a unique list of names of all the ingredients that this chef uses" do
+
+    visit "/chefs/#{@ramsey.id}"
+
+    click_link "View Chef's Ingredients"
+    
+    expect(current_path).to eq("/#{@ramsey.dishes.igredients}/")
+  end
 end
-# When I click on that link
-# I'm taken to a chef's ingredient index page
-# and I can see a unique list of names of all the ingredients that this chef uses
